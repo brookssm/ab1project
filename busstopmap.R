09cfaee2fab6277d293a2e44e86bd6198c8366c8
Seth Brooks <brookssm@uw.edu>
2018-05-30 01:16:20 -0700

    add neighborhoods and popups to map

--- busstopmap.R
+++ busstopmap.R
@@ -5,30 +5,24 @@ library(dplyr)
 library(ggplot2)
 library(ggmap)
 library(mapdata)
-
-#reads in bus stop data
-all_stops <- read.csv("./data/bus_stops_data.csv", stringsAsFactors = FALSE)
-
+library(geojsonio)
 library(leaflet)
-# loads map data
-map <- leaflet(all_stops) %>% addTiles('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', 
-                              attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, 
-                              <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy;
-                              <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>') 
-# sets color palette by agency
-agencycolor <- colorFactor(c("#5dff00", "#ED1B2E", "#006aff"), 
-                           all_stops$agency)
-# creates map
-map %>% 
-  # location of map and zoom
-  setView(-122.3035, 47.6553, zoom = 11) %>% 
-  # adds bus stop, assigns size and color
-  addCircles(~lon, ~lat, popup = paste0(all_stops$agency, ", ", 
-                                        all_stops$route_id), 
+
+topoData <- geojsonio::geojson_read("./data/geojson/neighborhoods.geojson",
+                                    what = "sp")
+leaflet(topoData) %>%
+  setView(-122.3035, 47.6553, zoom = 11) %>%
+  addTiles('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', 
+           attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, 
+                    <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy;
+                    <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>') %>% 
+  addPolygons(stroke = TRUE, smoothFactor = 0.1, fillOpacity = 0.1,
+              label = ~topoData$name, color = "white", 
+              weight = 1, opacity = 0.5) %>% 
+  addCircles(data = all_stops, ~lon, ~lat, popup = paste0(all_stops$agency, ", ", 
+                                                          all_stops$route_id), 
              weight = 10, radius = 50, color = ~agencycolor(agency), 
-             stroke = FALSE, fillOpacity = 0.8) %>% 
-  # adds legend for point color
+             stroke = FALSE, fillOpacity = 0.8) %>%
   addLegend("bottomright", pal = agencycolor, values = ~all_stops$agency,
             title = "Transit Agency",
-            opacity = 0.8
-  )
+            opacity = 0.8, data = all_stops)
--- data/seattle-boundaries-data-master/seattle-boundaries-data-master/example.js
+++
@@ -1,3 +0,0 @@
-var boundaries = require('./index')
-
-console.log(boundaries(-122.345002, 47.667044))
